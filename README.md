c018560f0a520673a9abd541c3d99ac1958742f2
Tanvir Alam Syed <tanviralamsyed@gmail.com>
2024-08-18 14:08:10 +0100

    changed from AI face detection to item detection :)

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Welcome to Face Recognition 
 This repo is just demonstrating the use of https://teachablemachine.withgoogle.com/, as I just wanted to mess around with it :)
-Hope you enjoy the repo, more will come 
+
+# How to run the code:
+
+- Before trying the code, you must first go to the link above and create a keras model and use the image model. 
+- Train a model to pick up which way you are looking and make sure the labels for each motion is sensible. 
+- Once that is done, download the kersa model ![Image for reference](image.png) ,and put the compressed file into the repo.
+- Decompress the file, which should leave with you two new files, a keras_model file and labels.txt.
+- now, run ' pip install -r requirement.txt ', this will download all required libaries you need.
+- once everything runs smoothly, run this command ' streamlit run app.py' 
+
--- main.py
+++ main.py
@@ -2,53 +2,46 @@ from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
 import streamlit as st
+from PIL import Image
 
 def main():
-     st.header("Chat with your PDF💬")
+     st.header("Item Detection")
      np.set_printoptions(suppress=True)
-    # Load the model
-     model = load_model("keras_Model.h5", compile=False)
-     class_names = open("labels.txt", "r").readlines()
-
-    # CAMERA can be 0 or 1 based on default camera of your computer
-     camera = cv2.VideoCapture(0)
-
-     while True:
-        # Grab the webcamera's image.
-        ret, image = camera.read()
-
-        # Resize the raw image into (224-height,224-width) pixels
-        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-
-        # Show the image in a window
-        cv2.imshow("Webcam Image", image)
-
-        # Make the image a numpy array and reshape it to the models input shape.
-        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-
-        # Normalize the image array
+     image = st.camera_input(label ="Capture Image", key="First Camera", label_visibility="hidden")
+     if image:
+        np.set_printoptions(suppress=True)
+        model = load_model("keras_Model.h5", compile=False)
+        class_names = open("labels.txt", "r").readlines()
+        
+        img = Image.open(image)
+        img_array = np.array(img)
+        
+        image = cv2.resize(img_array, (224, 224),interpolation=cv2.INTER_AREA)
+        st.image(image)
+        image = np.asarray(image).reshape(1,224, 224, 3)
         image = (image / 127.5) - 1
-
         # Predicts the model
         prediction = model.predict(image)
         index = np.argmax(prediction)
         class_name = class_names[index]
         confidence_score = prediction[0][index]
+        print(class_name)
+        print(confidence_score)
+        if confidence_score > 0.7:
+            if index == 0:
+                st.subheader("Ferrari")
+                st.write("Ferrari is an iconic Italian luxury sports car manufacturer renowned for its high-performance vehicles and rich racing heritage. Founded by Enzo Ferrari in 1939, the company has become a symbol of speed, elegance, and exclusivity. With a strong presence in Formula One, Ferrari continues to push the boundaries of automotive engineering and design, creating some of the most coveted cars in the world.")
+            if index == 1:
+                st.subheader("Pagani")
+                st.write("Pagani is an Italian luxury sports car manufacturer renowned for crafting exquisite, high-performance vehicles. Founded by Horacio Pagani, the company is celebrated for its meticulous attention to detail, innovative use of carbon fiber, and powerful engines. Pagani cars are considered works of art, with each model a limited-production masterpiece.")
+            if index ==  2:
+                st.subheader("Batmobile")
+                st.write("The Batmobile is Batman's iconic vehicle, a high-tech masterpiece housed in the Batcave. It's more than just a car; it's a heavily armored, versatile tool used for pursuit, capture, and combat. Packed with advanced weaponry and gadgets, it's a symbol of Batman's technological prowess and a fearsome deterrent to Gotham City's criminals. Its design has evolved over the years, but its core purpose remains the same: to aid the Dark Knight in his crusade for justice.")
+            print("Class:", class_name[2:], end="")
+            st.write("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+        else:
+            st.warning("unsure of itemm,try another one")
 
-        # Print prediction and confidence score
-        print("Class:", class_name[2:], end="")
-        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-
-        # Listen to the keyboard for presses.
-        keyboard_input = cv2.waitKey(1)
-
-        # 27 is the ASCII for the esc key on your keyboard.
-        if keyboard_input == 27:
-            break
-     camera.release()
-     cv2.destroyAllWindows()
-    
-    
 
 if __name__ == '__main__':
     main()
